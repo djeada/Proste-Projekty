85442a940d7e1d6ee9273be2b991a51b8720353b
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2024-03-10 14:25:40 +0100

    Create setup.py

---
+++ dodatkowe_materialy/szablony_projektow/python_setup_py/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='example_package',
+    version='0.1',
+    package_dir={'': 'src'},  # This line is added to indicate the source directory
+    packages=find_packages(where='src'),  # Specify the source directory
+    license='MIT',
+    description='A Python package example',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/yourusername/example_package',
+    author='Your Name',
+    author_email='your.email@example.com'
+)

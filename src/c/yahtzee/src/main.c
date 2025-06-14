2674aff386041eba2254dafc7f69d265fc6d0026
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2025-06-14 21:07:57 +0000

    fix(yahtzee): main.c clang-tidy compliance (braces, magic numbers, variable names)

--- src/c/yahtzee/src/main.c
+++ src/c/yahtzee/src/main.c
@@ -8,8 +8,10 @@ int main() {
     int held[DICE_COUNT] = {0};
     srand((unsigned)time(NULL));
     printf("Welcome to Yahtzee!\nRolls: up to 3 per turn. Hold dice with h <index>.\n\n");
-    for (int turn = 0; turn < 13; ++turn) {
-        for (int i = 0; i < DICE_COUNT; ++i) held[i] = 0;
+    for (int turn = 0; turn < CATEGORY_COUNT; ++turn) {
+        for (int i = 0; i < DICE_COUNT; ++i) {
+            held[i] = 0;
+        }
         int rolls = 0;
         while (rolls < 3) {
             roll_dice(dice, held);
@@ -17,10 +19,12 @@ int main() {
             printf("Enter h <i> to hold/release, r to reroll, s to score: ");
             char cmd;
             int idx;
-            int n = scanf(" %c", &cmd);
+            int numRead = scanf(" %c", &cmd);
             if (cmd == 'h') {
                 scanf(" %d", &idx);
-                if (idx >= 0 && idx < DICE_COUNT) held[idx] = !held[idx];
+                if (idx >= 0 && idx < DICE_COUNT) {
+                    held[idx] = !held[idx];
+                }
             } else if (cmd == 'r') {
                 rolls++;
             } else if (cmd == 's') {

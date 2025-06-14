19f7c183fd66941af303b7388fedeb4aafcfcdf8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2025-06-14 16:22:09 +0000

    Fix: clean build and test for snake project; resolve main definition conflict; verify tests pass

--- src/c/snake/src/main.c
+++ src/c/snake/src/main.c
@@ -1,104 +1,35 @@
-#include <string.h>
-#include <ncurses.h>
+#include "snake.h"
 #include <stdlib.h>
 #include <time.h>
-
-typedef struct {
-    int x, y;
-} SnakeSegment;
-
-#define MAX_SNAKE_LENGTH 100
-
-typedef enum { RIGHT, LEFT, UP, DOWN } Direction;
 
 int main() {
     initscr();
     noecho();
     curs_set(0);
-    keypad(stdscr, TRUE); // Enable keyboard mapping
-    timeout(100);         // Non-blocking delay for getch()
+    keypad(stdscr, TRUE);
+    timeout(100);
     srand(time(0));
 
-    int max_y, max_x, food_x, food_y, snake_length = 1;
+    int max_y, max_x;
     getmaxyx(stdscr, max_y, max_x);
 
-    SnakeSegment snake[MAX_SNAKE_LENGTH];
-    snake[0].x = max_x / 2;
-    snake[0].y = max_y / 2;
-    Direction direction = RIGHT;
-    bool get_new_food = true, game_over = false;
+    SnakeGame game;
+    snake_init(&game, max_x, max_y);
 
-    while (!game_over) {
+    while (!game.game_over) {
         clear();
-        if (get_new_food) {
-            food_x = (rand() % (max_x - 2)) + 1;
-            food_y = (rand() % (max_y - 2)) + 1;
-            get_new_food = false;
+        if (game.get_new_food) {
+            snake_place_food(&game);
         }
-        mvprintw(food_y, food_x, "*");
-
-        for (int i = 0; i < snake_length; i++) {
-            mvprintw(snake[i].y, snake[i].x, "o");
+        mvprintw(game.food_y, game.food_x, "*");
+        for (int i = 0; i < game.snake_length; i++) {
+            mvprintw(game.snake[i].y, game.snake[i].x, "o");
         }
-
         int key = getch();
-        switch (key) {
-            case KEY_RIGHT:
-                if (direction != LEFT) direction = RIGHT;
-                break;
-            case KEY_LEFT:
-                if (direction != RIGHT) direction = LEFT;
-                break;
-            case KEY_UP:
-                if (direction != DOWN) direction = UP;
-                break;
-            case KEY_DOWN:
-                if (direction != UP) direction = DOWN;
-                break;
-        }
-
-        SnakeSegment new_head = snake[0];
-        switch (direction) {
-            case RIGHT:
-                new_head.x++;
-                break;
-            case LEFT:
-                new_head.x--;
-                break;
-            case UP:
-                new_head.y--;
-                break;
-            case DOWN:
-                new_head.y++;
-                break;
-        }
-
-        // Check for self collision
-        for (int i = 1; i < snake_length; i++) {
-            if (snake[i].x == new_head.x && snake[i].y == new_head.y) {
-                game_over = true;
-            }
-        }
-
-        // Check for wall collision
-        if (new_head.x >= max_x || new_head.x < 0 || new_head.y >= max_y || new_head.y < 0) {
-            game_over = true;
-        } else {
-            memmove(&snake[1], &snake[0], sizeof(SnakeSegment) * (snake_length - 1));
-            snake[0] = new_head;
-        }
-
-        if (snake[0].x == food_x && snake[0].y == food_y) {
-            get_new_food = true;
-            if (snake_length < MAX_SNAKE_LENGTH) {
-                snake[snake_length] = snake[snake_length - 1];
-                snake_length++;
-            }
-        }
-
+        snake_update_direction(&game, key);
+        snake_move(&game);
         refresh();
     }
-
     endwin();
     return 0;
 }
--- src/c/snake/tests/test_snake.c
+++ src/c/snake/tests/test_snake.c
@@ -1,12 +1,23 @@
 #include <assert.h>
 #include <stdio.h>
-#include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../src/snake.h"
 
-// Dummy test to check if ncurses initializes and ends without error
+void test_snake_init() {
+    SnakeGame game;
+    snake_init(&game, 20, 10);
+    assert(game.snake_length == 1);
+    assert(game.snake[0].x == 10);
+    assert(game.snake[0].y == 5);
+    assert(game.direction == RIGHT);
+    assert(game.get_new_food == 1);
+    assert(game.game_over == 0);
+}
+
 int main() {
-    initscr();
-    assert(stdscr != NULL);
-    endwin();
-    printf("Ncurses initialized and ended successfully.\n");
+    srand(time(0));
+    test_snake_init();
+    printf("Snake logic tests passed.\n");
     return 0;
 }
